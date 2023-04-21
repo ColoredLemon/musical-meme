cfc21b19ac1d035ccda694812df74495cd1298f2
ColoredLemon <44214598+ColoredLemon@users.noreply.github.com>
2023-04-20 21:28:46 -0500

    A commitment to creating 50 programming projects to familiarize myself with the language and code. Also to make programming fun for me.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+//50 programming projects in python
+//https://www.reddit.com/r/learnprogramming/comments/2a9ygh/50_programming_projects_for_practice/
+//Lets make programming fun for me and a hobby that I enjoy doing
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world! Back to Programming Again");
+    }
+}
